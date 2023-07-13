f6f8d2c73b87f936f36da94f17aa8b677001e489
A Dipner Reddy <89031602+dipnerreddy@users.noreply.github.com>
2023-07-13 23:07:11 +0530

    Add files via upload

---
+++ RIP2000.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class RIP2000 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the No.of 2000Rs notes : ");
+		
+		int N=sc.nextInt();
+		System.out.println(N*4);
+    }
+}
